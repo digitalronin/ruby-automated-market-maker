8f48d7a7931183601f2bc4c3b2e9f44942b3b96e
digitalronin <david@digitalronin.com>
2021-04-21 18:32:56 +0900

    Add a spec that the pool stays balanced

--- spec/amm_spec.rb
+++ spec/amm_spec.rb
@@ -17,4 +17,21 @@ describe Amm do
   it "sets tokens value" do
     expect(amm.token_reserve).to eq(token_reserve)
   end
+
+  it "returns to initial state" do
+    tokens = amm.buy(1)
+    amm.sell(tokens)
+
+    actual = {
+      ether_reserve: amm.ether_reserve,
+      token_reserve: amm.token_reserve,
+    }
+
+    expected = {
+      ether_reserve: params[:ether_reserve],
+      token_reserve: params[:token_reserve],
+    }
+
+    expect(actual).to eq(expected)
+  end
 end

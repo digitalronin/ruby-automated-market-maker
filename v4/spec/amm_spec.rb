ed13bd77a8ecb2866518ae2488aa4c1494669bb2
digitalronin <david@digitalronin.com>
2021-05-01 13:16:51 +0900

    Separate existing specs into a context

--- v4/spec/amm_spec.rb
+++ v4/spec/amm_spec.rb
@@ -7,66 +7,68 @@ describe Amm do
 
   subject(:amm) { described_class.new(silent: true) }
 
-  before do
-    amm.add_liquidity(ether_reserve, token_reserve)
-  end
+  context "With initial liquidity" do
+    before do
+      amm.add_liquidity(ether_reserve, token_reserve)
+    end
 
-  it "sets eth value" do
-    expect(amm.ether_reserve).to eq(ether_reserve)
-  end
+    it "sets eth value" do
+      expect(amm.ether_reserve).to eq(ether_reserve)
+    end
 
-  it "sets tokens value" do
-    expect(amm.token_reserve).to eq(token_reserve)
-  end
+    it "sets tokens value" do
+      expect(amm.token_reserve).to eq(token_reserve)
+    end
 
-  it "returns to initial state" do
-    tokens = amm.buy(alice, 1)
-    amm.sell(alice, tokens)
+    it "returns to initial state" do
+      tokens = amm.buy(alice, 1)
+      amm.sell(alice, tokens)
 
-    actual = {
-      ether_reserve: amm.ether_reserve,
-      token_reserve: amm.token_reserve,
-    }
+      actual = {
+        ether_reserve: amm.ether_reserve,
+        token_reserve: amm.token_reserve,
+      }
 
-    expected = {
-      ether_reserve: ether_reserve,
-      token_reserve: token_reserve,
-    }
+      expected = {
+        ether_reserve: ether_reserve,
+        token_reserve: token_reserve,
+      }
 
-    expect(actual).to eq(expected)
-  end
+      expect(actual).to eq(expected)
+    end
 
-  it "increases token price" do
-    tokens1 = amm.buy(alice, 1)
-    tokens2 = amm.buy(alice, 1)
+    it "increases token price" do
+      tokens1 = amm.buy(alice, 1)
+      tokens2 = amm.buy(alice, 1)
 
-    expect(tokens1).to be > tokens2
-  end
+      expect(tokens1).to be > tokens2
+    end
 
-  it "reduces token price" do
-    tokens = amm.buy(alice, 1)
+    it "reduces token price" do
+      tokens = amm.buy(alice, 1)
 
-    eth1 = amm.sell(alice, tokens/2)
-    eth2 = amm.sell(alice, tokens/2)
+      eth1 = amm.sell(alice, tokens/2)
+      eth2 = amm.sell(alice, tokens/2)
 
-    expect(eth1).to be > eth2
-  end
+      expect(eth1).to be > eth2
+    end
 
-  it "takes ether from counterparty" do
-    amm.buy(alice, 1)
-    expect(alice.ether).to eq(initial_eth - 1)
-  end
+    it "takes ether from counterparty" do
+      amm.buy(alice, 1)
+      expect(alice.ether).to eq(initial_eth - 1)
+    end
 
-  it "gives tokens to counterparty" do
-    amm.buy(alice, 1)
-    expect(alice.tokens).to be > 0
-  end
+    it "gives tokens to counterparty" do
+      amm.buy(alice, 1)
+      expect(alice.tokens).to be > 0
+    end
 
-  it "takes tokens from counterparty" do
-    amm.buy(alice, 1)
-    t1 = alice.tokens
+    it "takes tokens from counterparty" do
+      amm.buy(alice, 1)
+      t1 = alice.tokens
 
-    amm.sell(alice, 1)
-    expect(alice.tokens).to eq(t1 - 1)
+      amm.sell(alice, 1)
+      expect(alice.tokens).to eq(t1 - 1)
+    end
   end
 end

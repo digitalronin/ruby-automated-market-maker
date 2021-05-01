009fd7f431d1ccd072049db098c10f5c25d2ad11
digitalronin <david@digitalronin.com>
2021-05-01 17:15:34 +0900

    Refactor REPL loop to be more concise

--- v4/bin/amm.rb
+++ v4/bin/amm.rb
@@ -16,18 +16,15 @@ loop do
 
   case string.chomp
   when /(.*) buy (.*)/
-    name = $1
+    counterparty = counterparties.find($1)
     ether = $2.to_f
-    counterparty = counterparties.find(name)
     amm.buy(counterparty, ether)
   when /(.*) sell (.*)/
-    name = $1
-    counterparty = counterparties.find(name)
+    counterparty = counterparties.find($1)
     tokens = $2 == "all" ? counterparty.tokens : $2.to_f
     amm.sell(counterparty, tokens)
   when /(.*) add_liquidity (.*) (.*)/, /(.*) add (.*) (.*)/
-    name = $1
-    counterparty = counterparties.find(name)
+    counterparty = counterparties.find($1)
     amm.add_liquidity(counterparty, $2.to_f, $3.to_f)
   when "counterparties", "c"
     counterparties.output

0cb437cb571c97a9993c1e983a885ef6958f2433
digitalronin <david@digitalronin.com>
2021-05-03 15:28:38 +0900

    Add 'lp_tokens' attribute to Counterparty

--- v5/lib/counterparty.rb
+++ v5/lib/counterparty.rb
@@ -1,9 +1,10 @@
 class Counterparty
-  attr_accessor :name, :ether, :tokens
+  attr_accessor :name, :ether, :tokens, :lp_tokens
 
   def initialize(params)
     @name = params.fetch(:name)
     @ether = params.fetch(:ether, 0).to_f
     @tokens = params.fetch(:tokens, 0).to_f
+    @lp_tokens = params.fetch(:lp_tokens, 0).to_f
   end
 end
